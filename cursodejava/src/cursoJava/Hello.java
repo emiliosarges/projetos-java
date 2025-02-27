3c04741397b6a53f64a26c1218b772376259b207
Emílio Silva <emiliosarges@gmail.com>
2025-02-26 23:30:46 -0300

    Initial commit

---
+++ cursodejava/src/cursoJava/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author emiliosilva
+ * Hello World
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
